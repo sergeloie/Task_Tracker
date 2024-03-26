e61119a679fcd60a7840d2637daa44cb104be2ca
serge loie <serge.loie@yandex.ru>
2024-03-26 17:18:19 +0500

    fix checkstyle errors

--- src/main/java/hexlet/code/app/controller/WelcomeController.java
+++ src/main/java/hexlet/code/app/controller/WelcomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/welcome")
-public class WelcomeController {
+public final class WelcomeController {
 
     @GetMapping
     public String welcome() {
