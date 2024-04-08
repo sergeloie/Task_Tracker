b514cb3989470304e0cff079a6c83300676bff59
daBoss <serge.loie@yandex.ru>
2024-04-08 23:03:32 +0500

    update User.java

---
+++ src/main/java/hexlet/code/app/model/User.java
@@ -0,0 +1,46 @@
+package hexlet.code.app.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @Email
+    @NotNull
+    private String email;
+
+    @NotNull
+    @Size(min = 3)
+    private String password;
+    @CreatedDate
+    private LocalDate createdAt;
+
+    private LocalDate updatedAt;
+
+}
