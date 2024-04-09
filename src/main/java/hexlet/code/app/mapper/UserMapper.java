d3940e58655d0895a2746a9bc5e6d319ce284701
daBoss <serge.loie@yandex.ru>
2024-04-09 20:11:40 +0500

    add UserMapper

---
+++ src/main/java/hexlet/code/app/mapper/UserMapper.java
@@ -0,0 +1,24 @@
+package hexlet.code.app.mapper;
+
+import hexlet.code.app.dto.UserCreateDTO;
+import hexlet.code.app.dto.UserDTO;
+import hexlet.code.app.dto.UserUpdateDTO;
+import hexlet.code.app.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(
+        uses = {JsonNullableMapper.class, ReferenceMapper.class},
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+public abstract class UserMapper {
+
+    public abstract User map(UserCreateDTO userCreateDTO);
+    public abstract UserDTO map(User user);
+    public abstract void update(UserUpdateDTO userUpdateDTO, @MappingTarget User user);
+}
