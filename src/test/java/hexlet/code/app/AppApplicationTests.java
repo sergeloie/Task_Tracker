2b261bb1fd31477123404d20e20069d8b40bdea6
daBoss <serge.loie@yandex.ru>
2024-04-14 21:23:40 +0500

    add index and create test

--- src/test/java/hexlet/code/app/AppApplicationTests.java
+++ src/test/java/hexlet/code/app/AppApplicationTests.java
@@ -1,11 +1,94 @@
 package hexlet.code.app;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hexlet.code.app.controller.UserController;
+import hexlet.code.app.model.User;
+import hexlet.code.app.repository.UserRepository;
+import hexlet.code.app.utils.ModelGenerator;
+import net.datafaker.Faker;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
 
 @SpringBootTest
+@AutoConfigureMockMvc
+
 class AppApplicationTests {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserController userController;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private Faker faker;
+
+    @Autowired
+    private ModelGenerator modelGenerator;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor token;
+
+    private User testUser;
+
+    @BeforeEach
+    public void setUp() {
+        token = jwt().jwt(builder -> builder.subject("hexlet@example.com"));
+        testUser = Instancio.of(modelGenerator.getUserModel()).create();
+        userRepository.save(testUser);
+    }
+
     @Test
     void contextLoads() {
     }
+
+    @Test
+    void someTest() {
+
+    }
+
+    @Test
+    void testIndex() throws Exception {
+        mockMvc.perform(get("/api/users")).andExpect(status().isOk());
+    }
+
+    @Test
+    void testCreate() throws Exception {
+        var data = Instancio.of(modelGenerator.getUserModel()).create();
+
+        var request = post("/api/users")
+                .with(token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(data));
+        mockMvc.perform(request)
+                .andExpect(status().isCreated());
+
+        var user = userRepository.findByEmail(data.getEmail());
+
+        assertNotNull(user);
+        assertThat(user.getFirstName()).isEqualTo(data.getFirstName());
+        assertThat(user.getLastName()).isEqualTo(data.getLastName());
+    }
+
 }
