edf84c5aff023574b06b9fda9e0769d1be4d6017
daBoss <serge.loie@yandex.ru>
2024-04-14 22:17:58 +0500

    add update test

--- src/test/java/hexlet/code/app/AppApplicationTests.java
+++ src/test/java/hexlet/code/app/AppApplicationTests.java
@@ -1,5 +1,6 @@
 package hexlet.code.app;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hexlet.code.app.controller.UserController;
 import hexlet.code.app.model.User;
@@ -13,14 +14,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import java.util.HashMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -48,7 +53,7 @@ class AppApplicationTests {
     @Autowired
     private ObjectMapper objectMapper;
 
-    private SecurityMockMvcRequestPostProcessors.JwtRequestPostProcessor token;
+    private JwtRequestPostProcessor token;
 
     private User testUser;
 
@@ -56,7 +61,9 @@ class AppApplicationTests {
     public void setUp() {
         token = jwt().jwt(builder -> builder.subject("hexlet@example.com"));
         testUser = Instancio.of(modelGenerator.getUserModel()).create();
+        System.out.println("testUser before save -> " + testUser);
         userRepository.save(testUser);
+        System.out.println("testUser after save -> " + testUser);
     }
 
     @Test
@@ -70,25 +77,43 @@ class AppApplicationTests {
 
     @Test
     void testIndex() throws Exception {
-        mockMvc.perform(get("/api/users")).andExpect(status().isOk());
+        mockMvc.perform(get("/api/users").with(jwt())).andExpect(status().isOk());
     }
 
     @Test
     void testCreate() throws Exception {
-        var data = Instancio.of(modelGenerator.getUserModel()).create();
+        User data = Instancio.of(modelGenerator.getUserModel()).create();
 
-        var request = post("/api/users")
+        MockHttpServletRequestBuilder request = post("/api/users")
                 .with(token)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(data));
         mockMvc.perform(request)
                 .andExpect(status().isCreated());
 
-        var user = userRepository.findByEmail(data.getEmail());
+        User user = userRepository.findByEmail(data.getEmail());
 
         assertNotNull(user);
         assertThat(user.getFirstName()).isEqualTo(data.getFirstName());
         assertThat(user.getLastName()).isEqualTo(data.getLastName());
+    }
+
+    @Test
+    void testUpdate() throws Exception {
+        HashMap<String, String> data = new HashMap<>();
+        data.put("firstName", "Mike");
+
+        MockHttpServletRequestBuilder request = put("/api/users/" + testUser.getId())
+                .with(token)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(data));
+
+
+        mockMvc.perform(request)
+                .andExpect(status().isOk());
+
+        User user = userRepository.findById(testUser.getId()).get();
+        assertThat(user.getFirstName()).isEqualTo("Mike");
     }
 
 }
