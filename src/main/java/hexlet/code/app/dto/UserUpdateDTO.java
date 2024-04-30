6de813788a663c3319a4207a71caf62acfb979ee
anseranser <serge.loie@yandex.ru>
2024-04-30 14:36:57 +0500

    checkstyle fix

--- src/main/java/hexlet/code/app/dto/UserUpdateDTO.java
+++ src/main/java/hexlet/code/app/dto/UserUpdateDTO.java
@@ -1,7 +1,6 @@
 package hexlet.code.app.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
