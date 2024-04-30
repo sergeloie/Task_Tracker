84ad5a46fa01253a407c72ff9c0bf57c8cdc9eb6
anseranser <serge.loie@yandex.ru>
2024-04-30 13:30:17 +0500

    add getCurrentUser()

---
+++ src/main/java/hexlet/code/app/utils/UserUtils.java
@@ -0,0 +1,24 @@
+package hexlet.code.app.utils;
+
+import hexlet.code.app.model.User;
+import hexlet.code.app.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor(onConstructor_ = @__(@Autowired))
+public class UserUtils {
+
+    private UserRepository userRepository;
+
+    public User getCurrentUser() {
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return null;
+        }
+        var username = authentication.getName();
+        return userRepository.findByEmail(username).get();
+    }
+}
