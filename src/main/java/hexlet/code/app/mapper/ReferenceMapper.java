3b06eaf74686890b1e1f77ff56dffc30db18faf8
serge loie <serge.loie@yandex.ru>
2024-05-02 16:52:26 +0500

    create mapping for Task

--- src/main/java/hexlet/code/app/mapper/ReferenceMapper.java
+++ src/main/java/hexlet/code/app/mapper/ReferenceMapper.java
@@ -1,11 +1,13 @@
 package hexlet.code.app.mapper;
 
 import hexlet.code.app.model.BaseEntity;
+import hexlet.code.app.model.TaskStatus;
 import jakarta.persistence.EntityManager;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.Named;
 import org.mapstruct.TargetType;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -20,5 +22,12 @@ public class ReferenceMapper {
 
     public <T extends BaseEntity> T toEntity(Long id, @TargetType Class<T> entityClass) {
         return id != null ? entityManager.find(entityClass, id) : null;
+    }
+
+    @Named("statusToTaskStatus")
+    public TaskStatus statusToTaskStatus(String status) {
+        return entityManager.createQuery("SELECT ts FROM TaskStatus ts WHERE ts.slug = :status", TaskStatus.class)
+                .setParameter("status", status)
+                .getSingleResult();
     }
 }
---
+++ src/main/java/hexlet/code/app/mapper/TaskMapper.java
@@ -0,0 +1,36 @@
+package hexlet.code.app.mapper;
+
+import hexlet.code.app.dto.task.TaskCreateDTO;
+import hexlet.code.app.dto.task.TaskDTO;
+import hexlet.code.app.dto.task.TaskUpdateDTO;
+import hexlet.code.app.model.Task;
+import org.mapstruct.*;
+
+@Mapper(
+        uses = {JsonNullableMapper.class, ReferenceMapper.class},
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        componentModel = MappingConstants.ComponentModel.SPRING,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+
+public abstract class TaskMapper {
+
+    @Mapping(target = "title", source = "name")
+    @Mapping(target = "content", source = "description")
+    @Mapping(target = "status", source = "taskStatus.slug")
+    @Mapping(target = "assigneeId", source = "assignee.id")
+    public abstract TaskDTO map(Task task);
+
+
+    @Mapping(target = "name", source = "title")
+    @Mapping(target = "description", source = "content")
+    @Mapping(target = "taskStatus", source = "status", qualifiedByName = "statusToTaskStatus")
+    @Mapping(target = "assignee", source = "assigneeId")
+    public abstract Task map(TaskCreateDTO taskCreateDTO);
+
+    @Mapping(target = "name", source = "title")
+    @Mapping(target = "description", source = "content")
+    @Mapping(target = "taskStatus", source = "status", qualifiedByName = "statusToTaskStatus")
+    @Mapping(target = "assignee", source = "assigneeId")
+    public abstract void update(TaskUpdateDTO taskUpdateDTO, @MappingTarget Task task);
+}
