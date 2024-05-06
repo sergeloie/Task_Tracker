980c721cc3fc11e303f4e57d67013b1538c82285
daBoss <serge.loie@yandex.ru>
2024-05-07 00:28:00 +0500

    add @JsonProperty("email") to username

--- src/main/java/hexlet/code/app/dto/user/UserUpdateDTO.java
+++ src/main/java/hexlet/code/app/dto/user/UserUpdateDTO.java
@@ -1,5 +1,6 @@
 package hexlet.code.app.dto.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -11,6 +12,7 @@ public class UserUpdateDTO {
     private String firstName;
     private String lastName;
     @Email
+    @JsonProperty("email")
     private String username;
     @Size(min = 3)
     private String password;
