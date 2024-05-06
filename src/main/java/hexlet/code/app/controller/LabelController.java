602479352f9316b24a5b5c1ca862f548e5fd6d82
daBoss <serge.loie@yandex.ru>
2024-05-07 00:28:59 +0500

    add pagination

--- src/main/java/hexlet/code/app/controller/LabelController.java
+++ src/main/java/hexlet/code/app/controller/LabelController.java
@@ -11,7 +11,11 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +23,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,10 +37,22 @@ public class LabelController {
     private LabelMapper labelMapper;
 
     @GetMapping
-    public List<LabelDTO> index() {
-        return labelRepository.findAll().stream()
+    public ResponseEntity<List<LabelDTO>> index(@RequestParam(defaultValue = "0") int _start,
+                                                @RequestParam(defaultValue = "10") int _end,
+                                                @RequestParam(defaultValue = "id") String _sort,
+                                                @RequestParam(defaultValue = "ASC") String _order) {
+        int page = _start / (_end - _start);
+        Sort.Direction direction = Sort.Direction.fromString(_order);
+        Pageable pageable = PageRequest.of(page, _end - _start, Sort.by(direction, _sort));
+
+        var result =  labelRepository.findAll(pageable).stream()
                 .map(labelMapper::map)
                 .toList();
+
+        return ResponseEntity.ok()
+                .header("X-Total-Count", String.valueOf(result.size()))
+                .body(result);
+
     }
 
     @GetMapping(path = "/{id}")
